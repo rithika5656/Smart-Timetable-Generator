232ddc33137f71c2da7d96543f91d1b143d54531
rithika5656 <rithika0164@gmail.com>
2026-01-30 12:44:47 +0530

    config

--- config.py
+++ config.py
@@ -18,3 +18,5 @@ MAX_TEACHERS = 20
 # Algorithm Config
 START_HOUR = 9  # 9 AM
 DEFAULT_DAYS = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"]
+BREAK_AFTER_PERIOD = 3  # Insert break after every 3 periods
+ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
