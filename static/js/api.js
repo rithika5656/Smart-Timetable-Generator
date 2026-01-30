6e10ca1d75312065f47734ebf52c005e3d296c2f
rithika5656 <rithika0164@gmail.com>
2026-01-30 16:03:06 +0530

    api.js

--- static/js/api.js
+++ static/js/api.js
@@ -2,6 +2,11 @@
  * API interaction layer.
  */
 
+/**
+ * Generate a new timetable.
+ * @param {Object} payload - The generation request parameters.
+ * @returns {Promise<Object>} The generated timetable result.
+ */
 export async function generateTimetable(payload) {
     const response = await fetch('/generate', {
         method: 'POST',
