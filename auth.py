6085e006d50fa048a35bcd0ed36a5e072c2fbcf2
rithika5656 <rithika0164@gmail.com>
2026-01-30 12:54:45 +0530

    auth

---
+++ auth.py
@@ -0,0 +1,14 @@
+"""
+Simple Authentication Logic.
+"""
+from functools import wraps
+from flask import session, redirect, url_for, flash, request
+from config import DEBUG
+
+def login_required(f):
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        if 'admin_logged_in' not in session:
+            return redirect(url_for('admin.login', next=request.url))
+        return f(*args, **kwargs)
+    return decorated_function
