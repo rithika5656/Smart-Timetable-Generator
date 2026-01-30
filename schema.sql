77fe7c52d6141ed95663bbe02107b60b2ff1c7a5
rithika5656 <rithika0164@gmail.com>
2026-01-30 12:49:01 +0530

    schema

---
+++ schema.sql
@@ -0,0 +1,9 @@
+CREATE TABLE IF NOT EXISTS history (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    timestamp DATETIME DEFAULT CURRENT_TIMESTAMP,
+    subjects TEXT NOT NULL,
+    teachers TEXT NOT NULL,
+    periods INTEGER NOT NULL,
+    duration REAL,
+    status TEXT
+);
