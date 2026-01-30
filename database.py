0fb9c2ff5c50885098739d171d4169cbbd49b831
rithika5656 <rithika0164@gmail.com>
2026-01-30 15:58:23 +0530

    database

--- database.py
+++ database.py
@@ -7,6 +7,7 @@ from flask import g
 DATABASE = 'scheduler.db'
 
 def get_db():
+    """Get the current database connection."""
     db = getattr(g, '_database', None)
     if db is None:
         db = g._database = sqlite3.connect(DATABASE)
@@ -14,15 +15,18 @@ def get_db():
     return db
 
 def close_connection(exception):
+    """Close the database connection at end of request."""
     db = getattr(g, '_database', None)
     if db is not None:
         db.close()
 
 def query_db(query, args=(), one=False):
+    """Execute a query and return results."""
     cur = get_db().execute(query, args)
     rv = cur.fetchall()
     cur.close()
     return (rv[0] if rv else None) if one else rv
 
 def init_app(app):
+    """Register database teardown context."""
     app.teardown_appcontext(close_connection)
